196929f0516f72f1729135306faf5a82b5ace1bf
ahorod <prozerpina112009@gmail.com>
2017-07-23 23:31:58 -0700

    add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :order_items
+
+  validates :name, presence: true
+  validates :description, presence: true, length: {minimum: 20, maximum: 200}
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@ class Seed
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop,
+      image_url: 'https://gloimg.rosewholesale.com/ROSE/2015/201507/source-img/1436466658259-P-2819655.jpg')
     end
   end
 end
